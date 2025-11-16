0091a3e2798a20e6c97f4ccfee36b728ee1a3599
MarquezPaulaKristha <21-02792@g.batstate-u.edu.ph>
2025-11-17 00:13:21 +0800

    update

--- node-server/server.js
+++ node-server/server.js
@@ -27,7 +27,6 @@ const pool = new Pool({
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
-  ssl: { rejectUnauthorized: false },
 });
 
 // --------------------
